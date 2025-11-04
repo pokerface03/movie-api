0f81dcbc9e81c95393abc9f3b8c1761682bbf1f8
pokerface03 <fparmakli@convertgroup.com>
2025-11-04 15:39:16 +0200

    CORS set up and code improvement

--- backend/controller.py
+++ backend/controller.py
@@ -1,21 +1,25 @@
 from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
 import csv
 import os
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 CSV_FILE = "movies.csv"
 
-if not os.path.exists(CSV_FILE):
-    with open(CSV_FILE, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["title", "director"])
-
 @app.get("/movies")
-def get_movies(director: str = Query("", alias="director")):
+def get_movies(director: str = Query("", alias="q")):
     with open(CSV_FILE, "r") as f:
         reader = csv.DictReader(f)
-        movies = [row for row in reader if director.lower() in row["director"].lower()]
+        movies = [row for row in reader if director.lower() in row["Director"].lower()]
     return movies
 
 @app.post("/movies")
